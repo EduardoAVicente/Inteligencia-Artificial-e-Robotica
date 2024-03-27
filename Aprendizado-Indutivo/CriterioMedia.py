5cbc8c6dda5df29db2693c1b5f9071a142de007f
EduardoAVicente <eduav22003@gmail.com>
2024-03-27 20:26:45 -0300

    Organização de arquivos

--- Aprendizado-Indutivo/CriterioMedia.py
+++ Aprendizado-Indutivo/CriterioMedia.py
@@ -10,24 +10,23 @@ import matplotlib.pyplot as plt
 from scipy.io import arff
 
 
-data,meta = arff.loadarff('CriterioProvas.arff')
+data,meta = arff.loadarff('Base-de-dados/CriterioMedia.arff')
 
 attributes = meta.names()
 data_value = np.asarray(data)
 
 
-p1 = np.asarray(data['P1']).reshape(-1,1)
-p2 = np.asarray(data['P2']).reshape(-1,1)
-faltas = np.asarray(data['PercFalta']).reshape(-1,1)
+percFalta = np.asarray(data['PercFalta']).reshape(-1,1)
+notas = np.asarray(data['Nota']).reshape(-1,1)
 
-features = np.concatenate((p2 , p1,faltas),axis=1)
+features = np.concatenate((notas , percFalta),axis=1)
 target = data['resultado']
 
 
 Arvore = DecisionTreeClassifier(criterion='entropy').fit(features, target)
 
 plt.figure(figsize=(10, 6.5))
-tree.plot_tree(Arvore,feature_names=['P1','P2','PercFalta'],class_names=['Aprovado', 'Reprovado'],
+tree.plot_tree(Arvore,feature_names=['Notas','PercFalta'],class_names=['Aprovado', 'Reprovado'],
                    filled=True, rounded=True)
 plt.show()
 
---
+++ Aprendizado-Indutivo/ExemploBank.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from sklearn import tree, metrics
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+from scipy.io import arff
+
+data = arff.loadarff('Base-de-dados/bank.arff')
+df = pd.DataFrame(data[0])
+df['subscribed'] = df['subscribed'].apply(lambda x: x.decode('utf-8'))
+
+features = df.drop('subscribed', axis=1)
+target = df['subscribed']
+
+categorical_columns = ['job', 'marital', 'education', 'default', 'housing', 'loan', 'contact', 'month', 'poutcome']
+for column in categorical_columns:
+    features[column] = pd.Categorical(features[column])
+    features[column] = features[column].cat.codes
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
+
+Arvore = DecisionTreeClassifier(criterion='entropy', max_depth=5, random_state=42)
+Arvore.fit(X_train, y_train)
+
+plt.figure(figsize=(25,10))
+tree.plot_tree(Arvore, feature_names=list(features.columns), class_names=['yes', 'no'], filled=True, rounded=True)
+plt.show()
+
+fig, ax = plt.subplots(figsize=(25, 10))
+metrics.plot_confusion_matrix(Arvore, X_test, y_test, display_labels=['yes', 'no'], values_format='d', ax=ax)
+plt.show()
